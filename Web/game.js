b6d88e58069ef51470e86755b5df64c30694e6e4
andrewiankidd <contact@andrewiankidd.co.uk>
2026-06-02 21:48:55 +0000

    Deploying to gh-pages from @ andrewiankidd/legacy-lua-25d@66abc0eb4ab9a0efec5b4a6bef87d91cb040a022 🚀

--- Web/game.js
+++ Web/game.js
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"2fa27833-cba1-49cc-be83-b7495db560ea","remote_package_size":1121330,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":1121330,"audio":false}]});
+  loadPackage({"package_uuid":"436e3df2-2c8c-4b9d-861a-db9b0457577b","remote_package_size":1121932,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":1121932,"audio":false}]});
 
 })();
